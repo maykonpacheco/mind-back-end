fba15684163817118e293015337432d0bad4a311
Maykon Pacheco <maykonpachecogeo@gmail.com>
2020-03-24 23:02:16 -0400

    Recuperação de senha

--- start/routes.js
+++ start/routes.js
@@ -3,6 +3,9 @@
 
 const Route = use('Route')
 
-Route.post('/users', 'UserController.store')
+Route.post('users', 'UserController.store')
 
-Route.post('/sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+
