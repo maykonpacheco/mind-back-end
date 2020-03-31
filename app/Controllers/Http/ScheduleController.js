e532d0b174b83bf87747b2f4fca61d2f9f3284d0
Maykon Pacheco <maykonpachecogeo@gmail.com>
2020-03-30 22:15:42 -0400

    Ajuste da funcionalidade para visualizar horario de atendimento

--- app/Controllers/Http/ScheduleController.js
+++ app/Controllers/Http/ScheduleController.js
@@ -1,57 +1,54 @@
 "use strict";
 
-const { startOfHour, parseISO, isBefore } = require("date-fns")
+const { startOfHour, parseISO, isBefore } = require("date-fns");
 
-const Schedule = use("App/Models/Schedule")
-
+const Schedule = use("App/Models/Schedule");
 
 class ScheduleController {
   async index({ params, response, view }) {
-
     const schedule = await Schedule.query()
-      .with("pacient_id", params.pacients_id).with('user')
+      .where("pacient_id", params.pacients_id)
+      .with("user")
       .fetch();
 
     return schedule;
   }
 
   async store({ params, request, response, auth }) {
-    
-    const { user_id, date } = request.body
+    const { user_id, date } = request.body;
 
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
       return response
         .status(400)
-        .send({ error: { message: "Essa data já passou." } })
+        .send({ error: { message: "Essa data já passou." } });
     }
 
-    const data = request.only(["date", "time", "value"])
-    
+    const data = request.only(["date", "time", "value"]);
 
     const schedule = await Schedule.create({
       ...data,
       user_id: auth.user.id,
       pacient_id: params.pacients_id
-    })
+    });
 
-    return schedule
+    return schedule;
   }
 
   async show({ params }) {
-    const schedule = await Schedule.findOrFail(params.id)
+    const schedule = await Schedule.findOrFail(params.id);
 
-    return schedule
+    return schedule;
   }
 
   async update({ params, request, response }) {}
 
   async destroy({ params }) {
-    const schedule = await Schedule.findOrFail(params.id)
+    const schedule = await Schedule.findOrFail(params.id);
 
-    await schedule.delete()
+    await schedule.delete();
   }
 }
 
-module.exports = ScheduleController
+module.exports = ScheduleController;
